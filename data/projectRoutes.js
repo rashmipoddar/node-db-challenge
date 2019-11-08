0861b6d9f8017b1a12bb31f24a21d3e4716d1d5d
Rashmi <poddar.rashmi@gmail.com>
2019-11-08 13:43:54 -0500

    Added endpoints for creating and viewing resources

--- data/projectRoutes.js
+++ data/projectRoutes.js
@@ -1,6 +1,8 @@
-const route = require('express').Router()
+const route = require('express').Router();
 
 const projectsDb = require('./projectModel');
+const tasksDb = require('./taskModel');
+const resourcesDb = require('./resourceModel');
 
 route.get('/', (req, res) => {  
   projectsDb.get()
@@ -37,6 +39,40 @@ route.post('/', (req,res) => {
         res.status(500).send({message: 'The project could not be created'})
       })
   }
+});
+
+route.post('/:id/tasks', (req, res) => {
+  const projectId = req.params.id;
+  console.log(projectId);
+
+  const taskDetails = req.body;
+  console.log(taskDetails);
+
+  if(!taskDetails.description) {
+    res.status(400).send({message: 'The task description is required'});
+  } else {
+    tasksDb.add({...taskDetails, project_id: projectId})
+      .then(id => {
+        console.log(id);
+        res.status(201).send({message: 'Task created'});
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).send({message: 'The task could not be created'});
+      })
+  }
+});
+
+route.get('/:id/tasks', (req, res) => {
+  tasksDb.getByProjectId(req.params.id)
+    .then(tasks => {
+      console.log(tasks);
+      res.status(200).send(tasks);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(200).send({message: 'There was an error in getting tasks from the database'});
+    })
 });
 
 module.exports = route;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 
 const projectRoutes = require('./data/projectRoutes');
+const resourceRoutes = require('./data/resourceRoutes');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use('/projects', projectRoutes);
+server.use('/resources', resourceRoutes);
 
 server.get('/', (req, res) => {
   res.status(200).send('Testing');
